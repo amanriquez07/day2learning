810712fca7af9be4d68ad5774ed5706d8a279478
Manriquez <amanriquez4@cps.k12.il.us>
2024-04-12 12:07:49 -0500

    second day part 3

--- day2.py
+++ day2.py
@@ -1,70 +1,87 @@
-# Description: This file is for the second day of the python workshop
+# # Description: This file is for the second day of the python workshop
 
 
-# create variables for the following :
-# 1. age
-age= 42
+# # create variables for the following :
+# # 1. age
+# age= 42
 
-# 2. name
-name= "Bill"
-# 3. song
-song= "Happy"
-# 4. food
-food= "pizza"
-# 5. number
-number=100 #this is an integer
+# # 2. name
+# name= "Bill"
+# # 3. song
+# song= "Happy"
+# # 4. food
+# food= "pizza"
+# # 5. number
+# number=100 #this is an integer
 
-#now include the variables you just made print in the following...
-print(f'''Once upon a time, there was a {age} old coder named {name}.
-{name} liked to hum the song {song} while coding. It was so annoying that his teammates would
-throw {food} until {name} would stop singing.
-Still, {name} was the best coder on the team and could write {number} lines of code every day.
-Maybe {song} was {name}’s secret power?
-No one will ever know.''')
-# f string interpolation is a way to format strings in python
-# what is syntax? Is a way of writing code that is correct and follows the rules
-#in python
-     #name="John"
-    # var name= "John"- javascript syntax
-
-
-
-print("Giraffe \n academy")
-# \n makes a new line
-print("Giraffe \t academy")
-# \t makes a tab
+# #now include the variables you just made print in the following...
+# print(f'''Once upon a time, there was a {age} old coder named {name}.
+# {name} liked to hum the song {song} while coding. It was so annoying that his teammates would
+# throw {food} until {name} would stop singing.
+# Still, {name} was the best coder on the team and could write {number} lines of code every day.
+# Maybe {song} was {name}’s secret power?
+# No one will ever know.''')
+# # f string interpolation is a way to format strings in python
+# # what is syntax? Is a way of writing code that is correct and follows the rules
+# #in python
+#      #name="John"
+#     # var name= "John"- javascript syntax
 
 
 
-# What is syntax ? What is an algorithm?
-# what is a variable? What is a string?
-
-# strings are nothing but plain text
-# what does this do?
-print("Giraffe \n academy")
-
-# or this
-phrase = "python learning"
-print(phrase + "is cool") #concatenation or putting strings insade of variables
-#what does the + sign do? What is it called?
+# print("Giraffe \n academy")
+# # \n makes a new line
+# print("Giraffe \t academy")
+# # \t makes a tab
 
 
-#what if I wanted to get the length of a phrase?
-# pharse= str(phrase)
-print(f'the length of the phrase" +len(phrase)')
-declarationOfIndepndence = "We hold these truths to be self-evident, that all men are created equal, that they are endowed, by their Creator, with certain unalienable rights, that among these are life, liberty, and the pursuit of happiness."
-print(f'the length of the declaration is" + len(declarationOfIndepndence)')
-#len allows you to find the length of a string
+
+# # What is syntax ? What is an algorithm?
+# # what is a variable? What is a string?
+
+# # strings are nothing but plain text
+# # what does this do?
+# print("Giraffe \n academy")
+
+# # or this
+# phrase = "python learning"
+# print(phrase + "is cool") #concatenation or putting strings insade of variables
+# #what does the + sign do? What is it called?
+
+
+# #what if I wanted to get the length of a phrase?
+# # pharse= str(phrase)
+# print(f'the length of the phrase" +len(phrase)')
+# declarationOfIndepndence = "We hold these truths to be self-evident, that all men are created equal, that they are endowed, by their Creator, with certain unalienable rights, that among these are life, liberty, and the pursuit of happiness."
+# print(f'the length of the declaration is" + len(declarationOfIndepndence)')
+# #len allows you to find the length of a string
 
 #what if I wanted to make the letters in the variable upper case or lower?
-
-
-
+new_phrase = "welcome to day 2 part 3"
+print(len(new_phrase))
+print(new_phrase.upper())
+#.upper is a method that makes the string all uppercase
+# parenthesis are used to call methods
+print(new_phrase.lower())
+# .lower is a method that makes the string all lower case
 
 #what if I wanted to check and see if the phrase was all lower or upper?
 
 
 #What if I wanted to get one letter of the phrase
+print(new_phrase[0]) #prints the first letter as w?
+print(new_phrase[1])
+print(new_phrase[2])
+print(new_phrase[3])
+print(new_phrase[4])
+print(new_phrase[5])
+print(new_phrase[6])
+print(new_phrase[7])
+print(new_phrase[8])
+print(new_phrase[9])
+print(new_phrase[10])
+print(new_phrase[11])
+print(new_phrase[-1]) #prints the last letter of the phrase
 
 
 # The names you use when creating these labels need to follow a few rules:
@@ -77,7 +94,7 @@ print(f'the length of the declaration is" + len(declarationOfIndepndence)')
 
 
 
-# Working with numbers bold text
+# Working with numbers 
 # We'll learn about the following topics:
 # 1. Types of Numbers in Python
 # 2. Basic Arithmetic
@@ -85,21 +102,21 @@ print(f'the length of the declaration is" + len(declarationOfIndepndence)')
 
 
 # Addition
-
+print(2+2)
 # Subtraction
-
+print(2-5)
 # Multiplication
-
+print(2*3)
 # Division
-
+print(10/2)
 # Modulus
-
+print(10%3)
 # Exponentiation
-
+print(2**3)
 # Floor Division
-
+print(10//3)
 # Order of Operations followed in Python
-
+print(2*3+1)
 # You can use parentheses to specify the order in which you want operations to be performed.
 
 #to do more you need to import special math libraries from python
