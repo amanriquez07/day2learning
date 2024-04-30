a8210c2f1cdd84a3c29bf0e2fa57e6743b2dc14a
Manriquez <amanriquez4@cps.k12.il.us>
2024-04-30 11:56:33 -0500

    more conditionals

---
+++ day3.py
@@ -0,0 +1,45 @@
+# print(10>9) #is this true or false
+
+
+# broughtFood = True
+# print(broughtFood)
+
+# is_raining = True
+# if is_raining:
+#     print("Take an umbrella! ")
+
+# else:
+#     print("No umbrella needed. ")
+
+
+
+
+# temperature = 15
+# if temperature > 30:
+#     print("If its warm ")
+#     print("Drink water ")
+# elif temperature > 20:
+#     print("It's nice ")
+# else:
+#     print("It's cold ")
+# print("Done")
+
+
+
+
+
+# age = 12
+# message = "Eligible" if age >= 18 else "Not eligible" #terneray operator
+# print(message)
+
+
+
+
+high_income = False
+good_credit = True
+student = False
+
+if not student:
+    print("Eligible")
+else: 
+    print("Not eligible ")
